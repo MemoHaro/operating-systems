2284cb7274906923ffa0f2e5e648083a82c8c6d9
Stefan Eng <stefaneng13@gmail.com>
2013-11-17 13:30:42 -0800

    Finished project 3. My implementation of wc

--- Projects/project3/mywc.c
+++ Projects/project3/mywc.c
@@ -44,18 +44,45 @@ int main(int argc, char **argv)
 	  return 1;
 	}
       }
-    }
+    }  
     else {
       // Read our file
-      FILE *file = fopen(argv[i], "r");      
-      process_file(file);      
-      printf("\t%s\n", argv[i]);
-      fclose(file);
+      ++filecount;
+      FILE *file = fopen(argv[i], "r");
+      // Opened file fine
+      if(file) {
+	process_file(file);      
+	printf(" %s\n", argv[i]);
+	fclose(file);
+      }
+      else {
+	fprintf(stderr, "mywc: unable to open %s\n", argv[i]);
+	return 1;
+      }
     }
-    // Read from stdin
-    if(filecount == 0) {
-      process_file(stdin);      
+  }
+  // Read from stdin if we have no files passed.
+  if(filecount == 0) {
+    process_file(stdin);      
+    printf("\n");
+  }
+  // Need to print total
+  if(filecount > 1) {
+    // Did not pass flags, print all information
+    if(flags == 0x00) {
+      printf("%5d %5d %5d", total_lines, total_words, total_bytes);
     }
+
+    if(flags & LINES) {
+      printf("%5d", total_lines);    
+    }
+    if(flags & WORDS) {
+      printf("%5d", total_words);
+    }
+    if(flags & BYTES) {
+      printf("%5d", total_bytes);
+    }  
+    printf(" total\n");
   }
   return 0;
 }
@@ -66,43 +93,49 @@ void process_file(FILE *fp)
   int c, prev;
   int lines, words, bytes;
   lines = words = bytes = 0;
-  // just set prev to some non-whitespace character
-  prev = 'a';
-
+  prev = -1;
   while((c = fgetc(fp)) != EOF) {
+    // Test if we have a word
+    if(isspace(c) && prev > 0 && !isspace(prev)) {
+      ++words;
+    }
+    // Test if we have a line
     if (c == '\n') {
       ++lines;
     }
-    // Don't count the words until we hit a non-whitespace character
-    else if(isspace(c)) {
-      
-    }   
-    // When we see the next c
-    // This is wrong, figure out a better way to count words
-    else if(isspace(prev) && !isspace(c)) {
-      ++words;
-    }   
-    else {
-      
-    }
-    // Always increment bytes
+    // Keep track of previous element
     prev = c;
+    // Always increment bytes
     ++bytes;
   }
+  // Non empty file, check if we missed one word at the end
+  if (bytes > 0) {
+    // Character as the last character, count it as a word
+    if(!isspace(prev)) {
+      ++words;
+    }
+  }
+  // Add the local sums to the global total
   total_lines += lines;
   total_words += words;
   total_bytes += bytes;
 
-  if(flags & LINES) {
-    printf("%5d", lines);    
+  // Did not pass flags, print all information
+  if(flags == 0x00) {
+    printf("%5d %5d %5d", lines, words, bytes);
   }
-  if(flags & WORDS) {
-    printf("%5d", words);
-  }
-  if(flags & BYTES) {
-    printf("%5d", bytes);
-  }  
 
-  printf("\n");
+  // Flags got passed, handle them seperately
+  else {
+    if(flags & LINES) {
+      printf("%5d", lines);    
+    }
+    if(flags & WORDS) {
+      printf("%5d", words);
+    }
+    if(flags & BYTES) {
+      printf("%5d", bytes);
+    }  
+  }
   return;
 }
