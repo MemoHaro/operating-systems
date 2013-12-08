b655f980120f96f01bc97558bb3fabb0f32a1f33
Stefan Eng <stefaneng13@gmail.com>
2013-12-08 10:59:47 -0800

    Finished project 6

--- Projects/project6/mywc_fork.c
+++ Projects/project6/mywc_fork.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <ctype.h>
-// Added
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -21,20 +20,17 @@ struct count process_file(FILE *fp);
 
 int main(int argc, char *argv[])
 {
-  // keep track of which indices contain files
-  //  int file_index[argc];
+  int c, filecount;
 
-  int c;
-  //, filecount;
-  
   pid_t pid;
-  int pipefd[2];  
+  int pipefd[2];
   // Keep track of local count
-  struct count local_count;
+  struct count local_count = {.bytes = 0, .words = 0, .lines = 0};
   // keep track of total
-  //  struct count total;
-  
-  //  filecount = 0;
+  struct count total_count = {.bytes = 0, .words = 0, .lines = 0};
+  filecount = 0;
+
+  char *files[argc];
 
   // pipe
   if(pipe(pipefd)) {
@@ -43,12 +39,12 @@ int main(int argc, char *argv[])
 
   int i;
   // iterate through the arguments
-  for(i = 1; i < argc; i++) {   
+  for(i = 1; i < argc; i++) {
     // process arguments that are flags
-    if(*argv[i] == '-') {      
+    if(*argv[i] == '-') {
       // Iterate through the character in the flags
       // Options w, c, and l are supported.
-      while((c = *(++argv[i]))) {		
+      while((c = *(++argv[i]))) {
 	switch(c) {
 	case 'c':
 	  flags |= BYTES;
@@ -65,61 +61,69 @@ int main(int argc, char *argv[])
 	  return 1;
 	}
       }
-    }  
+    }
     else {
-      // For each file, fork the process      
-      //++filecount;
-      if((pid = fork()) == -1) {
-	fprintf(stderr, "Fork error.");
-	exit(1);
-      }
-      // Child
-      else if(pid == 0) {
-	// Close up input side, child does not need to read
-	close(pipefd[0]);
-	// Open up file for reading
-	FILE *file = fopen(argv[i], "r");
-	// Get the count for each file
-	local_count = process_file(file);	
-	// Write the struct of totals to the pipe
-	write(pipefd[1], &local_count, sizeof(struct count));
-	// Write out the name and process id
-	printf(" %s \tPID: %d\n", argv[i], getpid());
-	fclose(file);	
-	exit(0);
-      }
-      // Parent
-      else {	
-	// Holds the value of each read
-	struct count tmp;	
-	// Parent does not need to write
-	//close(pipefd[1]);
-	// Wait for all the children to exit
-	//int i;
-	//while(wait(NULL) > 0) {
-	wait(NULL);
-	  printf("Read %d bytes\n", (int)read(pipefd[0], &tmp, sizeof(struct count)));
-	  printf("Blah: %d %d %d\n", tmp.bytes, tmp.words, tmp.lines);
-	  //	}
-
-	//for(i = 0; i < filecount; i++) {
-	// wait(NULL);
-	//}	
-	//	printf("Read %d bytes\n", (int)read(pipefd[0], &tmp, sizeof(struct count)));
-	//	printf("Blah: %d %d %d\n", tmp.bytes, tmp.words, tmp.lines);
-	//	while(wait(NULL) > 0);
-	// Read from the pipe
-	//	while((i = (int)read(pipefd[0], &tmp, sizeof(struct count))) >= 0) {
-	//	  printf("Read %d bytes\n", i);
-	//	  printf("Blah: %d %d %d\n", tmp.bytes, tmp.words, tmp.lines);
-	//	}
-	// Done reading, close up read side
-	//	close(pipefd[0]);
-	//exit(0);
-      }
-    }    
+      // File up the array with the file names
+      files[filecount++] = argv[i];
+    }
   }
 
+  // Read from stdin if we have no files
+  if(filecount == 0) {
+    process_file(stdin);
+    printf("\n");
+    exit(0);
+  }
+
+  // Iterate over each file to write to pipe
+  int k;
+  for(k = 0; k < filecount; k++) {
+    if((pid = fork()) == -1) {
+      fprintf(stderr, "Error while forking.\n");
+      exit(1);
+    }
+    if(pid == 0) {
+      FILE *file = fopen(files[k], "r");
+      // Get the count for each file
+      local_count = process_file(file);
+      printf(" %s \tPID: %d\n", files[k], getpid());
+      if(write(pipefd[1], &local_count, sizeof(struct count)) == -1) {
+	fprintf(stderr, "Error writing to pipe.\n");
+      }
+      exit(0);
+    }
+  }
+
+  for(k = 0; k < filecount; k++) {
+    if(read(pipefd[0], &local_count, sizeof(struct count)) == -1) {
+      fprintf(stderr, "Error reading the pipe.\n");
+    }
+    total_count.bytes += local_count.bytes;
+    total_count.words += local_count.words;
+    total_count.lines += local_count.lines;
+  }
+
+  // Don't need to print total if just one file
+  if(filecount == 1) {
+    exit(0);
+  }
+
+  // Did not pass flags, print all information
+  if(flags == 0) {
+    printf("%5d ", total_count.lines);
+    printf("%5d ", total_count.words);
+    printf("%5d ", total_count.bytes);
+  }
+  if(flags & LINES) {
+    printf("%5d ", total_count.lines);
+  }
+  if(flags & WORDS) {
+    printf("%5d ", total_count.words);
+  }
+  if(flags & BYTES) {
+    printf("%5d ", total_count.bytes);
+  }
+  printf("total\n");
   return 0;
 }
 
@@ -127,7 +131,6 @@ int main(int argc, char *argv[])
 struct count process_file(FILE *fp)
 {
   int c, prev;
-  //  int lines, words, bytes;
   struct count filecount;
   filecount.lines = 0;
   filecount.words = 0;
@@ -156,21 +159,21 @@ struct count process_file(FILE *fp)
   }
 
   // Did not pass flags, print all information
-  if(flags == 0x00) {
+  if(flags == 0) {
     printf("%5d %5d %5d", filecount.lines, filecount.words, filecount.bytes);
   }
 
   // Flags got passed, handle them seperately
   else {
     if(flags & LINES) {
-      printf("%5d", filecount.lines);    
+      printf("%5d", filecount.lines);
     }
     if(flags & WORDS) {
       printf("%5d", filecount.words);
     }
     if(flags & BYTES) {
       printf("%5d", filecount.bytes);
-    }  
+    }
   }
   return filecount;
 }
